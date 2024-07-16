53e2c4dcad680dfdec631dea7308d46d7c1bf992
24sjv <144608951+24sjv@users.noreply.github.com>
2024-07-16 17:53:53 +0530

    Update ExWave.py

--- ExWave.py
+++ ExWave.py
@@ -1,5 +1,5 @@
 # HiddinWave Ver 1.0
-# Powered by TechChip
+
 # Secret Message Extractor
 import os
 import wave
@@ -27,9 +27,7 @@ def banner():
 | || (_)__| |__| |___ _ _ \ \    / /_ ___ _____
 | __ | / _` / _` / -_) ' \ \ \/\/ / _` \ V / -_)
 |_||_|_\__,_\__,_\___|_||_|_\_/\_/\__,_|\_/\___|
-                         |___|v1.0 \033[1;91mwww.techchip.net\033[0m
-\033[92mVisit for more tutorials : www.youtube.com/techchipnet\033[0m
-\033[93mHide your text message in wave audio file like MR.ROBOT\033[0m''')
+                         |___|
 
 def ex_msg(af):
     if not arged:
